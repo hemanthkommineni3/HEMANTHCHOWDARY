32c08c0614fb58e4262958838978292f175bf1dc
hemanthkommineni3 <146945443+hemanthkommineni3@users.noreply.github.com>
2025-04-18 16:28:15 +0530

    Add files via upload

---
+++ Program2.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Program2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int a = sc.nextInt();
+
+        for (int i = 0; i < a; i++) {
+            int odd = 2 * i + 1;
+            System.out.print(odd);
+            if (i < a - 1) {
+                System.out.print(", ");
+            }
+        }
+        System.out.println();  // move to next line after printing series
+        sc.close();
+    }
+}
